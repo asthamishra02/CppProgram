2a88cb5cab6613a20f1cc0a38f70525cd6ad0b57
asthamishra02 <astham2003@gmail.com>
2022-08-31 21:08:59 +0530

    addingnumber

---
+++ addingnumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+
+{
+	int a,i=0,s=0;
+	cout<<"Enter value of a ";
+	cin>>a;
+	for(i=1;i<=a;i++)
+	{
+	    s=s+i;
+	    
+	 }
+	 cout<<s;
+	return 0;
+}
