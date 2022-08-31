c2578f1a2850a5f209d3d00238ca104f341ab315
asthamishra02 <astham2003@gmail.com>
2022-08-31 19:06:31 +0530

    7up

---
+++ 7up7down.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout<<"Enter number:";
+    cin>>n;
+    if(n==7)
+    {
+        cout<<"Number is 7";
+    }
+    else if(n>7)
+    {
+        cout<<" 7 up";
+    }
+    else
+    {
+        cout<< "7 down";
+    }
+
+
+}
